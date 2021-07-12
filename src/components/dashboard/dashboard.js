32e75bbfc3887f912884d1f518c554efbaffbf19
unknown <>
2021-07-12 22:46:12 +0530

    dashboard's navbar

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,16 +1,50 @@
 import React from "react";
+import "./dashboard.css";
+import { Navbar, Nav, NavDropdown} from "react-bootstrap";
 
 function Dashboard(){
 return (
-<div id="dashboard">
-        dashboard
-        hey hi bye seeu wowowowo
-        <br />
-        <br />
-        <a href="../createPortfolio/create">
-        <button > clicl me</button>
-        </a>
-      </div>
+<div class="dashb">
+
+<Navbar bg="dark" expand="lg" fixed='top'>
+  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link href="#action1">Home</Nav.Link>
+      <Nav.Link href="#action2">Link</Nav.Link>
+      <NavDropdown title="Link" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#" disabled>
+        Link
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+
+  {/* <header>
+      <h1>resuMAMA</h1>
+  </header> */}
+  <footer>
+      <p>Copyright ⓒ </p>
+    </footer>
+</div>
       );
 }
 export default Dashboard;
+
+
+
+
+
+
+
