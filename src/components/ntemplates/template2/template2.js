cfe62ed52f1512fe592c99496a78913ef4647233
Sonia <sonia.v007@yahoo.in>
2021-07-20 03:45:56 +0530

    Experience section in Template created

--- src/components/ntemplates/template2/template2.js
+++ src/components/ntemplates/template2/template2.js
@@ -1,12 +1,5 @@
 import React from "react";
 import "./template2.css";
-// import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-// import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
-// import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
-// import VisibilityIcon from '@material-ui/icons/Visibility';
-// import ShareIcon from '@material-ui/icons/Share';
-// import EditIcon from '@material-ui/icons/Edit';
-
 import PhoneIcon from '@material-ui/icons/Phone';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -16,7 +9,7 @@ import $ from "jquery";
 
 function Template1(){
 
-    function phoneview(){
+    function phoneview(){                             //for navbar
     // console.log("sdsjfhjf");
     // $('.Temp1navItem').toggleClass("show");
     }
@@ -28,15 +21,15 @@ function Template1(){
                 <nav class="temp1nav" >
                     <ul >
                         <li class="Temp1Logo">Sonia</li>
-                        <li class="Temp1navItem"> <a href="#">About</a></li>
-                        <li class="Temp1navItem"> <a href="#">Skills</a></li>
-                        <li class="Temp1navItem"> <a href="#">Projects</a></li>
-                        <li class="Temp1navItem"> <a href="#">Experience</a></li>
-                        <li class="Temp1navItem"> <a href="#">Contact</a></li>
+                        <li class="Temp1navItem"> <a href="#t1head">About</a></li>
+                        <li class="Temp1navItem"> <a href="#t1skill">Skills</a></li>
+                        <li class="Temp1navItem"> <a href="#t1project">Projects</a></li>
+                        <li class="Temp1navItem"> <a href="#t1experience">Experience</a></li>
+                        <li class="Temp1navItem"> <a href="#t1contact">Contact</a></li>
                         <li class="menuIcon" onClick={phoneview}><a href="#"><MenuIcon/></a></li>
                     </ul>
                 </nav>
-                <div class="temp1main">
+                <div class="temp1main" id="t1head">
                     <div class="contentdiv">
                         <h1> Sonia Verma Suhasini</h1>
                         <div class="aboutcontent">I am an awesome Swimming Instructor. The ocean is my freedom! 
@@ -58,7 +51,7 @@ function Template1(){
                 </div>
                 </div>
 
-                <div class="temp1skill"> 
+                <div class="temp1skill" id="t1skill"> 
                 <div class="skilldiv">
                 <div class="skilltag">Skills</div> 
                 <div class="Grid-div">
@@ -111,18 +104,68 @@ function Template1(){
                                 
                 </div>
 
-                <div class="temp1proj"> 
+                <div class="temp1proj" id="t1project"> 
         
-                
+                <div class="projdiv">
+                <div class="projtag">Projects</div> 
+
+
+                 </div>
+
                 </div>
 
-                <div class="temp1experience">            
-                
+                <div class="temp1experience" id="t1experience">            
+                    <div class="expdiv">
+                    <div class="exptag">Experience</div>
 
-                
+                    <div class="container">
+
+   
+                        <div class="timeline-block timeline-block-right">
+                            <div class="marker"></div>
+                            <div class="timeline-content">
+                                <h3>First Year</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </div>
+
+                        <div class="timeline-block timeline-block-left">
+                            <div class="marker"></div>
+                            <div class="timeline-content">
+                                <h3>Seconed Year</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </div>
+
+                        <div class="timeline-block timeline-block-right">
+                            <div class="marker"></div>
+                            <div class="timeline-content">
+                                <h3>Third Year</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </div>
+
+                        <div class="timeline-block timeline-block-left">
+                            <div class="marker"></div>
+                            <div class="timeline-content">
+                                <h3>Fourth Year</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </div>
+
+                        <div class="timeline-block timeline-block-right">
+                            <div class="marker"></div>
+                            <div class="timeline-content">
+                                <h3>Fifth Year</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            </div>
+                        </div>
+                        </div>
+                 
+                </div>
                 </div>
 
-                <div class="temp1foot"> 
+                <div class="temp1foot" id="t1contact"> 
                   <div class="contactIcons">
                   <PhoneIcon className="footIcon"></PhoneIcon>
                   <LinkedInIcon className="footIcon"></LinkedInIcon>
