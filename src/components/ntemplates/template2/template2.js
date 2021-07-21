3314131a6c1b9047c1e010990e3597f62749f2a8
Sonia <sonia.v007@yahoo.in>
2021-07-21 22:38:00 +0530

    html links commented

--- src/components/ntemplates/template2/template2.js
+++ src/components/ntemplates/template2/template2.js
@@ -129,7 +129,7 @@ function Template1(){
                             </div>
                         </div>
 
-                        {/* <div class="timeline-block timeline-block-left">
+                        <div class="timeline-block timeline-block-left">
                             <div class="marker"></div>
                             <div class="timeline-content">
                                 <h3>Seconed Year</h3>
@@ -160,9 +160,9 @@ function Template1(){
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
                             </div>
                         </div>
-                        </div> */}
                         </div>
-                </div>
+                        </div>
+                
                 </div>
 
                 <div class="temp1foot" id="t1contact"> 
